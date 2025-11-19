b68a4d942770061d20fcb0138c1004792df6dd46
Libertrendz <gustavo.santos@libertrendz.com.br>
2025-11-19 19:19:36 +0000

    Configure remote image patterns in next.config.mjs
    Added remote image patterns for Supabase storage.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ncglfknuaugpkmkbsiyf.supabase.co",
+        pathname: "/storage/v1/object/public/Imagens/**"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
